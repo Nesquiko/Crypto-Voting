64b7acbc830e15ad7064b6dfb0468e44684ff28f
Nesquiko <lukascastven@gmail.com>
2022-01-03 20:05:21 +0100

    Add choice to the VotingSession.
    Only an owner can add a choice to the VotingSession. Also tested the
    addChoice function.

--- contracts/VotingSession.sol
+++ contracts/VotingSession.sol
@@ -10,33 +10,31 @@ contract VotingSession is Ownable {
     string public symbol;
     uint256 public start;
     uint256 public end;
-    uint8 public numOfVotes;
+    uint8 public numOfVotesPerUser;
 
     string[] public choices;
     mapping(string => uint24) public numOfVotesPerChoice;
-
-    VotingHub private votingHub;
-    IERC20 private voteToken;
 
     constructor(
         string memory _symbol,
         uint256 _start,
         uint256 _end,
-        uint8 _numOfVotes,
-        address _voteToken,
-        address _votingHub
+        uint8 __numOfVotesPerUser
     ) {
         symbol = _symbol;
         start = _start;
         end = _end;
-        numOfVotes = _numOfVotes;
-        voteToken = IERC20(_voteToken);
-        votingHub = VotingHub(_votingHub);
+        numOfVotesPerUser = __numOfVotesPerUser;
     }
 
     function vote(string memory choice, uint8 numberOfVotes) public {}
 
-    function becomeVoter() public {}
+    function addChoice(string memory choice) public onlyOwner {
+        require(block.timestamp < end, "This voting session already ended.");
+
+        choices.push(choice);
+        numOfVotesPerChoice[choice] = 0;
+    }
 
     function getResults() public {}
 }
---
+++ tests/unit/test_voting_session.py
@@ -0,0 +1,87 @@
+import time
+import pytest
+
+from brownie.network.contract import Contract, ProjectContract
+from brownie.network.transaction import TransactionReceipt
+from brownie import VotingSession, exceptions
+
+from scripts.deploy_voting_hub import deploy_voting_hub
+from scripts.util import from_account, get_account
+
+
+def test_voting_session_creation():
+    account = get_account()
+    voting_hub: ProjectContract = deploy_voting_hub()
+
+    expected_symbol = "TEST"
+    start = int(time.time())
+    end = start + 10
+    num_votes = 2
+
+    tx: TransactionReceipt = voting_hub.createVotingSession(
+        expected_symbol, start, end, num_votes, from_account(account)
+    )
+    tx.wait(1)
+
+    voting_session = Contract.from_abi(
+        "VotingSession", tx.return_value, VotingSession.abi
+    )
+
+    assert voting_session.symbol() == expected_symbol
+    assert voting_session.start() == start
+    assert voting_session.end() == end
+    assert voting_session.numOfVotesPerUser() == num_votes
+
+
+def test_add_choice_from_owner():
+    account = get_account()
+    voting_hub: ProjectContract = deploy_voting_hub()
+
+    symbol = "Presidential Vote"
+    start = int(time.time())
+    end = start + 10000000
+    num_votes = 2
+
+    tx: TransactionReceipt = voting_hub.createVotingSession(
+        symbol, start, end, num_votes, from_account(account)
+    )
+    tx.wait(1)
+
+    voting_session = Contract.from_abi(
+        "VotingSession", tx.return_value, VotingSession.abi
+    )
+
+    choice = "Joe Biden"
+    tx = voting_session.addChoice(choice, from_account(account))
+    tx.wait(1)
+
+    assert voting_session.choices(0, from_account(account)) == choice
+    assert voting_session.numOfVotesPerChoice(choice, from_account(account)) == 0
+
+
+def test_add_choice_from_non_owner():
+    account = get_account()
+    voting_hub: ProjectContract = deploy_voting_hub()
+
+    symbol = "Presidential Vote"
+    start = int(time.time())
+    end = start + 10000000
+    num_votes = 2
+
+    tx: TransactionReceipt = voting_hub.createVotingSession(
+        symbol, start, end, num_votes, from_account(account)
+    )
+    tx.wait(1)
+
+    voting_session = Contract.from_abi(
+        "VotingSession", tx.return_value, VotingSession.abi
+    )
+
+    non_owner = get_account(index=1)
+    choice = "Joe Biden"
+
+    with pytest.raises(exceptions.VirtualMachineError):
+        tx = voting_session.addChoice(choice, from_account(non_owner))
+
+
+# TODO test time ended
