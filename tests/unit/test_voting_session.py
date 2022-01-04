9c582c3c7ebc4e28b9080aea317984a1360a423d
Nesquiko <lukascastven@gmail.com>
2022-01-04 16:42:34 +0100

    Non passing test Github Actions.

--- tests/unit/test_voting_session.py
+++ tests/unit/test_voting_session.py
@@ -57,7 +57,9 @@ def test_add_choice_from_owner():
     tx.wait(1)
 
     assert voting_session.choices(0, from_account(account)) == choice
-    assert voting_session.numOfVotesPerChoice(choice, from_account(account)) == 0
+    assert (
+        voting_session.numOfVotesPerChoice(choice, from_account(account)) == 1
+    )  # Only testing Github Actions
 
 
 def test_add_choice_from_non_owner():
