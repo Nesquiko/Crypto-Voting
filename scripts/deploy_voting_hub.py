8b1b0ec6d42ff315f8dc493adf8279d59bf681c6
Nesquiko <lukascastven@gmail.com>
2022-01-08 10:50:43 +0100

    Verification of contract.

--- scripts/deploy_voting_hub.py
+++ scripts/deploy_voting_hub.py
@@ -1,5 +1,6 @@
-from brownie import VotingHub
+from brownie import VotingHub, config, network
 from brownie.network.contract import ProjectContract
+
 
 from scripts.util import from_account, get_account
 
@@ -7,7 +8,10 @@ from scripts.util import from_account, get_account
 def deploy_voting_hub():
     account = get_account()
 
-    voting_hub: ProjectContract = VotingHub.deploy(from_account(account))
+    voting_hub: ProjectContract = VotingHub.deploy(
+        from_account(account),
+        publish_source=config["networks"][network.show_active()]["verify"],
+    )
 
     return voting_hub
 
